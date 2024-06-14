f19247f7aad174fa5fb11cce90ae3507a940d41f
root <root@next-hyd-9803.nexteducation.com>
2024-06-14 18:36:45 +0530

    Encapsulation assessment is Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+	public static void main(String[] args) {
+		RWOnly rwOnly = new RWOnly();
+		//rwOnly.name ="Krishna";
+		//System.out.println(rwOnly.name);
+		rwOnly.setName("Nanduri");
+		System.out.println(rwOnly.getName());
+	}
   
 }
